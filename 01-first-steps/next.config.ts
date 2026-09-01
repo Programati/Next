5e8fa6fe943ae4b8c3319cf6651c926ba03cca5b
matias <mati.j.martinez@gmail.com>
2026-08-31 23:13:12 -0300

    act

--- 01-first-steps/next.config.ts
+++ 01-first-steps/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Para subir a GitHub/Vercel hay que comentar esta linea del Docker
   // output: "standalone",
 };
 
